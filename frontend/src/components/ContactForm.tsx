4e141461f18302f1627a6fd5af047226a796cc67
SomeOne1305 <axmadxolmuminov2007@gmail.com>
2023-12-07 07:31:39 +0500

    some changes in contact

--- frontend/src/components/ContactForm.tsx
+++ frontend/src/components/ContactForm.tsx
@@ -1,9 +1,63 @@
 import React from "react";
+import Fetcher from "../api/Fetcher";
+import { useMutation } from "react-query";
 
 type Props = {};
+type FormValues = {
+  name:string,
+  email:string,
+  comment:string
+}
+type IErrMessages = {
+  email:string,
+  name:string,
+  surname:string,
+  comment:string
+}
+
+const validateFormData = (formData:IErrMessages, type:"email"|"comment"|"name"|"surname") : string => {
+  const { email, name, surname,comment } = formData;
+  const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/; // Email format regex
+  
+  // Error messages
+  const errorMessages :IErrMessages = {
+    email: "Please enter a valid email.",
+    name: "Please enter a valid name.",
+    surname: "Please enter a valid surname.",
+    comment:"Please do not leave comment box empty."
+  };
+  
+  if(type === 'name'){
+    if (!name || name.match(/[^a-zA-Z\s]/)) {
+      return errorMessages.name
+     }
+  }else if(type === 'surname'){
+    if (!surname || surname.match(/[^a-zA-Z\s]/)) {
+     return errorMessages.surname
+    }
+  }else if(type === 'email'){
+    if (!email || !email.match(emailRegex)) {
+      return errorMessages.email
+    }
+  }else if (type === 'comment'){
+    if(!comment){
+      return errorMessages.comment
+    }
+  }
+};
 
 const ContactForm = (props: Props) => {
- 
+  const sendComment = useMutation({
+    mutationFn:(request:FormValues)=>{
+      return Fetcher.post('/contact',request)
+    }
+  })
+
+  const handleSubmit = (e:React.FormEvent) =>{
+    e.preventDefault()
+    const payload = new FormData(e.target)
+    sendComment.mutate(payload)
+  }
   
   return (
     <section>
@@ -13,7 +67,7 @@ const ContactForm = (props: Props) => {
             Contact Us
           </h1>
         </div>
-        <form action="" method="post" className="max-w-screen-sm mx-auto">
+        <form onSubmit={(e:React.FormEvent)=>handleSubmit(e)} className="max-w-screen-sm mx-auto">
           <div className="w-full p-1 flex max-sm:flex-col">
             <div className="relative w-full p-1 max-sm:mr-0 mr-2">
               <input
@@ -21,6 +75,7 @@ const ContactForm = (props: Props) => {
                 id="name"
                 className="peer w-full bg-transparent border dark:focus:border-[#0ea5e9] rounded-md border-[#e2e8f0] dark:border-[#334155] p-2 placeholder:text-transparent outline-none focus:border-[2px] focus:border-[#0ea5e9]"
                 placeholder="name"
+                name="name"
               />
               <label
                 htmlFor="name"
@@ -35,6 +90,7 @@ const ContactForm = (props: Props) => {
                 id="surname"
                 className="peer bg-transparent dark:focus:border-[#0ea5e9] w-full dark:border-[#334155] border rounded-md border-[#e2e8f0] p-2 placeholder:text-transparent outline-none focus:border-[2px] focus:border-[#0ea5e9]"
                 placeholder="name"
+                name="surname"
               />
               <label
                 htmlFor="surname"
@@ -51,6 +107,7 @@ const ContactForm = (props: Props) => {
                 id="email"
                 className="peer bg-transparent w-full dark:focus:border-[#0ea5e9] dark:border-[#334155] border rounded-md border-[#e2e8f0] p-2 placeholder:text-transparent outline-none focus:border-[2px] focus:border-[#0ea5e9] invalid:border-pinkWarning invalid:text-pinkWarning"
                 placeholder="name"
+                name="email"
               />
               <label
                 htmlFor="email"
@@ -80,7 +137,7 @@ const ContactForm = (props: Props) => {
             </div>
           </div>
           <div className="flex justify-end p-1">
-            <button className="px-2 py-1 text-lg transition duration-500 hover:bg-none hover:text-[#12A2E9] border-[2px] border-transparent hover:border-[#12A2E9] active:ring active:ring-[#a78bfa] bg-first text-white rounded-md">
+            <button type="submit" className="px-2 py-1 text-lg transition duration-500 hover:bg-none hover:text-[#12A2E9] border-[2px] border-transparent hover:border-[#12A2E9] active:ring active:ring-[#a78bfa] bg-first text-white rounded-md">
               Send
             </button>
           </div>
